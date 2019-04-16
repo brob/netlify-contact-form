b80768de6532074a2e73b29e6a08f6390616ac8f
Bryan Robinson <bryanlrobinson@gmail.com>
2019-04-16 13:31:01 -0500

    Adds html template to sendgrid html

--- function-src/contact-route.js
+++ function-src/contact-route.js
@@ -4,14 +4,18 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
 export function handler(event, context, callback) {
-    console.log(JSON.parse(event.body).data);
     let form = JSON.parse(event.body).data;
+
+    let html = `<h1>Form submission from ${form.name}</h1>
+                <p>${form.message}</p>`;
+
+
     const msg = {
         to: form.department,
         from: form.email,
         subject: 'Contact Form Submission',
         text: form.message,
-        html: form.message,
+        html: html,
       };
 
       sgMail.send(msg);
