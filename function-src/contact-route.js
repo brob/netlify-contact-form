403e4d2373fd793f0462569ee1b224ae34a6db53
Bryan Robinson <bryanlrobinson@gmail.com>
2019-04-16 12:38:28 -0500

    Hooks up SendGrid for serverless function

--- function-src/contact-route.js
+++ function-src/contact-route.js
@@ -1,7 +1,19 @@
 'use strict';
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
 export function handler(event, context, callback) {
     console.log(JSON.parse(event.body).data);
+    let form = JSON.parse(event.body).data;
+    const msg = {
+        to: form.department,
+        from: form.email,
+        subject: 'Contact Form Submission',
+        text: form.message,
+        html: form.message,
+      };
+
+      sgMail.send(msg);
 
 }
