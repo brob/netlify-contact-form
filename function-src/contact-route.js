883f0f6ce03aefba7ce6dad1db56d636cf2f72bb
Bryan Robinson <bryanlrobinson@gmail.com>
2019-04-16 13:37:15 -0500

    Adds ternary and extra fields

--- function-src/contact-route.js
+++ function-src/contact-route.js
@@ -7,7 +7,9 @@ export function handler(event, context, callback) {
     let form = JSON.parse(event.body).data;
 
     let html = `<h1>Form submission from ${form.name}</h1>
-                <p>${form.message}</p>`;
+                <p><strong>Email Address:</strong> ${form.email}</p>
+                <p><strong>Phone Number:</strong> ${form.phone ? form.phone : 'Not Submitted'}</p>
+                <p><strong>Message:</strong> ${form.message}</p>`;
 
 
     const msg = {
